faa676c526e0b878032bd3a783ed064635dffd29
Benny <benny@Bennys-MacBook-Pro.local>
2023-10-26 13:31:18 +0200

    Set typescript server express.

--- server.ts
+++ server.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import express from "express";
+import chalk from 'chalk';
+
+const app = express();
+
+const port: number = 6969;
+
+// routes base and a responce
+/**
+ * Da qui possiamo mettere tutto il necessario per ricevere
+ * le richieste per  il server;
+*/
+
+app.get('/', (req, res) => {
+    res.send('ciao come vá?');
+});
+
+// start the server
+app.listen(port, () => {
+    console.log("\n*** server runnig: " + chalk.bgGreen(`http://localhost on port ${port}`) + " ***");
+});
